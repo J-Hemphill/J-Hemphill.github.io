43b9571effd12ba8bb62f66166f7a2971b85c70b
Jeffry Hemphill <99753925+J-Hemphill@users.noreply.github.com>
2023-06-03 18:12:28 -0500

    Changed project urls in WorkCardData.js

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -8,13 +8,13 @@ const ProjectCardData = [
         imgsrc: pro1,
         title: 'Point And Shoot Game',
         text: 'This is a point and shoot game developed using JavaScript, HTML, CSS, Sprite sheets, and SoundFX.',
-        demo: 'https://j-hemphill.github.io/JavaScript-Game-Development-1/'    
+        demo: 'https://point-and-shoot-javascript-game.netlify.app/'    
     },
     {
         imgsrc: pro2,
         title: 'Todo App',
         text: 'This app was developed using the MERN Stack. It is an app that allows the user to create a list of todos, update a todo, or delete a todo.',
-        demo: 'https://wondrous-croissant-cd94d5.netlify.app/'
+        demo: 'https://jh-todo-manager-app.netlify.app//'
     },
     {
         imgsrc: pro3,

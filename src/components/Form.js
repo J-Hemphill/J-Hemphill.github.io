e62c6b1b3e06afd5fe130d58955da847bb932011
Jeffry Hemphill <99753925+J-Hemphill@users.noreply.github.com>
2023-06-04 12:28:07 -0500

    Enabled contact page to send info.

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,44 @@
 import './FormStyles.css';
 
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Form = () => {
+
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userSubject, setUserSubject] = useState('');
+  const [userMessage, setUserMessage] = useState('');
+  const form = useRef('');
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setUserName('');
+    setUserEmail('');
+    setUserSubject('');
+    setUserMessage('');
+  
+    emailjs.sendForm('service_n5ya5wg', 'template_cx78kms', form.current, 'vLMnzsuLEMcl2pNjP')
+      .then((result) => {
+          console.log(result.text);
+          console.log('Message sent!');
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
   return (
     <div className='form'>
-        <form>
+        <form ref={form} onSubmit={sendEmail}>
             <label>Your Name</label>
-            <input type='text'></input>
+            <input type='text' name='user_name' value={userName} onChange={(e) => setUserName(e.target.value)}></input>
             <label>Your Email</label>
-            <input type='email'></input>
+            <input type='email' name='user_email' value={userEmail} onChange={(e) => setUserEmail(e.target.value)}></input>
             <label>Subject</label>
-            <input type='text'></input>
+            <input type='text' name='subject' value={userSubject} onChange={(e) => setUserSubject(e.target.value)}></input>
             <label>Message</label>
-            <textarea rows='6' placeholder='Type your message here...' />
-            <button className='btn'>Submit</button>
+            <textarea name='message' rows='6' placeholder='Type your message here...' value={userMessage} onChange={(e) => setUserMessage(e.target.value)}/>
+            <button className='btn' type='submit'>Submit</button>
         </form>
     </div>
   );
